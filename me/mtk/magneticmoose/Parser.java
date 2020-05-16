2f8597c753026347e4b1a8ece8be8a4bbded33e5
Matthew Kosloski <matthewkosloski@me.com>
2020-05-15 22:08:34 -0500

    added comments

--- me/mtk/magneticmoose/Parser.java
+++ me/mtk/magneticmoose/Parser.java
@@ -2,21 +2,41 @@ package me.mtk.magneticmoose;
 
 import java.util.List;
 
+// The Parser is the part of the interpreter that takes
+// a list of Token objects as input and, from those tokens, 
+// constructs an abstract syntax tree (AST). The construction
+// of the AST is based on the grammar of the language of 
+// the source program. That is, the Parser is an implementation
+// of the grammar. Each nonterminal symbol of the grammar
+// is implemented as a method.
 public class Parser 
 {
     private static class ParseError extends RuntimeException {}
 
+    // The tokens of the source program. These come from
+    // the Lexer.
     private final List<Token> tokens;
 
     // Represents the index in tokens of the next
     // token waiting to be parsed.
     private int current = 0;
 
+    /**
+     * Constructs a new Parser object, initializing
+     * it with a list of tokens.
+     * 
+     * @param tokens A list of tokens.
+     */
     public Parser(List<Token> tokens)
     {
         this.tokens = tokens;
     }
 
+    /**
+     * Parses the program.
+     * 
+     * @return A tree of expressions representing the program.
+     */
     public Expr parse()
     {
         try
@@ -29,13 +49,23 @@ public class Parser
         }
     }
 
-    // program -> expression ;
+    /*
+     * Implements the following production rule:
+     * program -> expression ;
+     *
+     * @return An expression.
+     */
     private Expr program()
     {
         return expression();
     }
 
-    // expression -> literal | arithmetic ;
+    /*
+     * Implements the following production rule:
+     * expression -> literal | arithmetic ;
+     *
+     * @return An expression.
+     */
     private Expr expression()
     {
         if (match(TokenType.NUMBER))
@@ -52,13 +82,23 @@ public class Parser
         throw error(peek(), "Expected expression.");
     }
 
-    // literal -> number ;
+    /*
+     * Implements the following production rule:
+     * literal -> number ;
+     *
+     * @return An expression.
+     */
     private Expr literal()
     {
         return new Expr.Literal((double) previous().literal);
     }
 
-    // arithmetic -> "(" operator expression (" " expression)+ ")" ;
+    /*
+     * Implements the following production rule:
+     * arithmetic -> "(" operator expression (" " expression)+ ")" ;
+     *
+     * @return An expression.
+     */
     private Expr arithmetic()
     {
         Token operator = advance();
@@ -119,5 +159,4 @@ public class Parser
         MagneticMoose.error(msg, token.line, token.column);
         return new ParseError();
     }
-
 }
